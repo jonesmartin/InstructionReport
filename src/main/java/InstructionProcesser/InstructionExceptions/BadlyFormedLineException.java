5e09894d6dc80c2f7215b1ecd4db919ed6490a20
MARTIN JONES <jones.martin@gmail.com>
2017-09-13 13:56:06 +0100

    Added constructors for completeness

--- src/main/java/InstructionProcesser/InstructionExceptions/BadlyFormedLineException.java
+++ src/main/java/InstructionProcesser/InstructionExceptions/BadlyFormedLineException.java
@@ -2,4 +2,19 @@ package InstructionProcesser.InstructionExceptions;
 
 public class BadlyFormedLineException extends Exception {
 
+    public BadlyFormedLineException() {
+        super();
+    }
+
+    public BadlyFormedLineException(String message) {
+        super(message);
+    }
+
+    public BadlyFormedLineException(String message, Throwable chained) {
+        super(message, chained);
+    }
+
+    public BadlyFormedLineException(Throwable chained) {
+        super(chained);
+    }
 }
--- src/main/java/InstructionProcesser/InstructionExceptions/MalformedCurrencyException.java
+++ src/main/java/InstructionProcesser/InstructionExceptions/MalformedCurrencyException.java
@@ -1,5 +1,19 @@
 package InstructionProcesser.InstructionExceptions;
 
 public class MalformedCurrencyException extends Exception {
+    public MalformedCurrencyException() {
+        super();
+    }
 
+    public MalformedCurrencyException(String message) {
+        super(message);
+    }
+
+    public MalformedCurrencyException(String message, Throwable chained) {
+        super(message, chained);
+    }
+
+    public MalformedCurrencyException(Throwable chained) {
+        super(chained);
+    }
 }
